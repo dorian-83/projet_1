7eb8c5997ff4c31140edf1a7373a8f1777f006d2
Leloup Dorian <dleloup@14i008.stud.mua>
2024-09-18 17:03:19 +0200

    début écriture module

--- minmax.py
+++ minmax.py
@@ -1,5 +1,6 @@
 #!/bin/env python3
 import sys
+from module_test.py import construire
 
 """
     Soit L le type liste dont les Ã©lÃ©ments sont soit tous de type int, soit tous de type L.
@@ -13,43 +14,23 @@ import sys
     La liste doit Ãªtre fournie sous la forme : [ [ 1 2 ] [ [ 2 3 4 ] [ 5 4 3 2 ] [ [ 3 1 ] [ 2 ] ] ] [ 0 9 ] ]
 """
 
-def construire():
-    def _construire():
-        """
-        Cette fonction rÃ©cursive construit la liste 
-        Ã  partir des arguments fournis sur la ligne de commande.
-        Elle retourne la liste construite.
-        """
-
-        nonlocal i  
-        l = []          
-        while True:
-            if sys.argv[i]=="[":   
-                i+=1               
-                if i!=2:                  # pour la premiÃ¨re liste, on ne fait rien
-                    l.append(_construire()) 
-            elif sys.argv[i]=="]":        # c'est la fin de la liste,
-                i+=1
-                return l                  # on renvoie la liste constuite
-            else:                         # c'est une liste d'entiers
-                l.append(int(sys.argv[i]))   
-                i+=1
-    i = 1                              # indice pour parcourir les arguments
-    return _construire()
-
 def minmax(l):
     """
     Cette fonction rÃ©cursive retourne le minmax de la liste passÃ©e en argument.
     """
-    if type(l[0])==int:
-        maxi.append(max(l))
-    else:
-        for i in l:
-            minmax(i)
+        if type(l[0])==int:
+            maxi.append(max(l))
+        else:
+            for i in l:
+                minmax(i)
 
 if __name__=="__main__":
-    # programme principal
-    l = construire()                   # rÃ©cupÃ©ration de la liste
-    maxi = []
-    minmax(l)
-    print(min(maxi))
+        if len(sys.argv)==1:
+            print("dommage")
+        else:
+            l=construire()
+            maxi = []
+            minmax(l)
+            print(min(maxi))
+
+   
