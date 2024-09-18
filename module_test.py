585c53ee2ed2abbe74e2e63ffbed64bb6df2abaa
Leloup Dorian <dleloup@14i008.stud.mua>
2024-09-18 16:33:06 +0200

    premier commit avant changement des programmes initiaux

---
+++ module_test.py
@@ -0,0 +1,25 @@
+#!/bin/env python3
+
+import re sys
+
+
+def arguments():                      #programme qui décide ce qui se passera en fonction du nombre d'arguments que l'on met
+	if len(sys.argv)==0
+		
+	elif nom de fichier donné:
+		
+	else la liste qu'on voudra traiter
+
+
+
+if sys.argv[i]
+
+if __name__=="__main__":
+    # programme principal
+    while True:
+        line = input("? ").rstrip("\n").strip()
+        if line=="":
+            break
+        lline = re.split(r' +',line.rstrip("\n"))
+
+
